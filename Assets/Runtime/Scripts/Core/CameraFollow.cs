5d9e96f7627fe56d67f6e7181057c038094d5a17
LeoFeitosa <>
2021-12-22 19:15:45 -0300

    add camera follow

---
+++ Assets/Runtime/Scripts/Core/CameraFollow.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    [SerializeField] float offSetX;
+
+    void LateUpdate()
+    {
+        Vector3 currentPosition = transform.position;
+        Vector2 playerPosition = player.transform.position;
+        currentPosition.x = playerPosition.x + offSetX;
+        transform.position = currentPosition;
+    }
+}
